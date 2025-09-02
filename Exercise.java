7780659e959ce5d726d07e46e54df9571b24ee0c
Laura Muñoz <lacrimugi.l.c@gmail.com>
2025-09-02 15:38:01 -0500

    Agrego notación Big O asociada

--- Exercise.java
+++ Exercise.java
@@ -1,10 +1,28 @@
 import java.util.Scanner; 
 
+/**
+ Calcula el número de Fibonacci en la posición n.
+
+ o La implementación Big(O) asociada es: O(n) porque este ciclo se repite n veces
+    lo que significa que crece de manera proporcional al tamaño de la entrada.
+
+ o La operación dominante es la suma dentro del bucle for 'c = a + b;''
+    porque es la que más se repite.
+**/
+
 public class Exercise{ 
-    public static int fibonacci(int n) { 
-        if (n <= 1) return n; 
-        return fibonacci(n - 1) + fibonacci(n - 2); 
+
+    public static long fibonacci(int n) {         
+        if (n <= 1) return n;  // Comparación y retorna constante O(1)
+        long a = 0, b = 1, c = 0;
         
+        // Complejidad: O(n)
+        for (int i = 2; i <= n; i++) {  // Bucle principal: se ejecuta (n-1) veces O(n)
+            c = a + b;  // Operación dominante Suma 
+            a = b;  // Asignación constante O(1)
+            b = c;  // Asignación constante O(1)
+        }
+        return b; // Retorno del resultado O(1)
     } 
         
     public static void main(String[] args) {
@@ -12,10 +30,12 @@ public class Exercise{
         System.out.print("Ingresa un valor para n: "); 
         int n = sc.nextInt(); 
         
+        // Medición del tiempo de ejecución
         long inicio = System.nanoTime();
         System.out.println("El número de Fibonacci para " + n + " es: " + fibonacci(n)); 
         long fin = System.nanoTime() ;
         
+        // Conversión del tiempo de nanosegundos a segundos
         double duracion = (fin -inicio)/1000000000.0;
         
         System.out.println("La duracion total es: " + duracion); 
--- Exercise.py
+++ Exercise.py
@@ -1,17 +1,33 @@
 import time
 
+"""
+Calcula el número de Fibonacci en la posición n
+
+o La implementación Big(O) asociada es: O(n) porque este ciclo se repite n veces
+    lo que significa que crece de manera proporcional al tamaño de la entrada.
+
+o  Operación dominante: la suma 'a + b'.
+    Esa suma se repite dentro del bucle tantas veces como el valor de n.
+"""
+
 def fibonacci(n):
     a, b = 0, 1
+
+    # El bucle se repite n veces
+    # Cada vuelta realiza la suma "a + b" (operación dominante)
     for _ in range(n):
         a, b = b, a + b
     return a
 
+# Entrada por teclado
 n = int(input("Ingresa un valor para n: "))
 
+# Medición del tiempo de ejecución
 inicio = time.perf_counter_ns()
 print(f'El número de Fibonacci para {n} es: {fibonacci(n)}')
 fin = time.perf_counter_ns()
 
+# Conversión de nanosegundos a segundos
 duracion = (fin -inicio)/1000000000.0
 
 print("La duracion total es: ", duracion)
